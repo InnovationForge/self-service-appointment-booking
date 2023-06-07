9a782a058f20caad37f76a93feffe176a07c89f8
Niranjan Rath <niranjan.rath@gmail.com>
2023-06-07 08:53:56 +0200

    first commit

---
+++ ssab-server/src/main/java/com/github/innovationforge/SsabServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.innovationforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsabServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsabServerApplication.class, args);
+	}
+
+}
